7296b49acc7ab0e9dcc5224d6f98c016622dcc88
lucashmorais <morais.lucas.h@gmail.com>
2015-04-17 16:31:44 -0300

    Including global header

---
+++ Global.h
@@ -0,0 +1,29 @@
+/*
+ * Global.h
+ *
+ *  Created on: Apr 13, 2015
+ *      Author: lucas
+ */
+
+#ifndef GLOBAL_H_
+#define GLOBAL_H_
+
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <vector>
+#include <thread>
+#include <unordered_map>
+#include <unordered_set>
+#include "Call.h"
+#include "DepAnalyst.h"
+#include "Function.h"
+#include "HashDepAnalyst.h"
+#include "MemReader.h"
+#include "RandomTaskDispatcher.h"
+#include "TaskDispatcher.h"
+#include "HashDepAnalyst.h"
+
+
+#endif /* GLOBAL_H_ */
